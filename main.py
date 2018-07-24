5780a73b395ea2a23d73d5011871982358130d19
EntangledLabs <33011062+EntangledLabs@users.noreply.github.com>
2018-07-24 09:12:47 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys, pygame
+from pygame.locals import *
 from PIL import Image
 img = Image.open('maze.png')
 background = [img]
 size = width, height = 180, 180
-screen = pygame.display.set_mode([180,180])
-
+screen = pygame.display.set_mode((180,180))
+background = pygame.image.load('maze.png').convert()
+screen.blit(background, (0, 0))
 # Plan coordinate system
 # Starting coordinates: (15-19, 0-4)
 # Ending coordinates: (45-49, 180-184)
@@ -21,22 +23,22 @@ print(colors)
 pix = img.load()
 list = []
 
-# Locate the starting coordinate
+# Locate the ending coordinate
 for x in range(0,180):
     if pix[x,0] == (255, 255, 255, 255):
         list.append(x)
 print(list[2])
 
-# Print the starting coordinate
+# Print the ending coordinate
 print(list[2],",",177)
 
 list = []
 
-# Locate the ending coordinate
+# Locate the starting coordinate
 for x in range(0,180):
     if pix[x,179] == (255, 255, 255, 255):
         list.append(x)
 print(list[2])
 
-# Locate the ending coordinate
-print (list[2],",",3)                                                                                
+# Locate the starting coordinate
+print (list[2],",",3)
