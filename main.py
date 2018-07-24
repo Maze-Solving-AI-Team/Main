2a08b631ab00ac7139b65a873fc42774a326c75d
EntangledLabs <33011062+EntangledLabs@users.noreply.github.com>
2018-07-24 11:09:28 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-import sys, pygame
+# Import modules
+import sys, pygame, time, math
+from time import sleep
 from pygame.locals import *
 from PIL import Image
+# Initialize
 img = Image.open('maze.png')
+change = 5
+width = img.width * change
+height = img.height * change
 background = [img]
-size = width, height = 180, 180
-screen = pygame.display.set_mode((180,180))
+screen = pygame.display.set_mode((width,height))
 background = pygame.image.load('maze.png').convert()
-screen.blit(background, (0, 0))
-# Plan coordinate system
-# Starting coordinates: (15-19, 0-4)
-# Ending coordinates: (45-49, 180-184)
-# Marker: 5px
-# Each coordinate: 5px by 5px
-
+newscreen = pygame.transform.scale(background, (width, height))
+screen.blit(newscreen, (0, 0))
+pygame.display.update()
+color = (0,128,0)
 
 # Recognizing black/white
-print(img.size)
+print(width, height)
 size = [img.size]
 print(size[0])
 colors = img.getcolors()
@@ -23,22 +25,29 @@ print(colors)
 pix = img.load()
 list = []
 
-# Locate the ending coordinate
-for x in range(0,180):
-    if pix[x,0] == (255, 255, 255, 255):
-        list.append(x)
-print(list[2])
-
-# Print the ending coordinate
-print(list[2],",",177)
-
-list = []
-
 # Locate the starting coordinate
 for x in range(0,180):
     if pix[x,179] == (255, 255, 255, 255):
         list.append(x)
-print(list[2])
 
-# Locate the starting coordinate
-print (list[2],",",3)
+xvalueOfStart = list[0] * change
+print(xvalueOfStart)
+
+blockSize = len(list) * change
+
+list = []
+
+# Locate the ending coordinate
+for x in range(0,180):
+    if pix[x,0] == (255, 255, 255, 255):
+        list.append(x)
+
+xvalueOfEnd = list[0] * change
+print(xvalueOfEnd)
+
+# Make maze appear on screen for set amount of seconds
+for x in range(0,100):
+    screen.blit(newscreen, (0, 0))
+    pygame.draw.rect(newscreen, color, pygame.Rect(xvalueOfStart, height - blockSize, 5 * change, 5 * change))
+    pygame.display.update()
+    time.sleep(0.1)
